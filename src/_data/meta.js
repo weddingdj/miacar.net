ba0b61a27f57fc28c49e4430b27e920da78bbc1c
Chris Car <chris@ideaspacevr.org>
2026-03-11 16:20:53 +0100

    added personal infos

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,18 +1,18 @@
 export const url = process.env.URL || 'http://localhost:8080';
 // Extract domain from `url`
 export const domain = new URL(url).hostname;
-export const siteName = 'Eleventy Excellent';
-export const siteDescription = 'Eleventy starter for building modern, resilient websites';
+export const siteName = 'Mia Car';
+export const siteDescription = 'Art by Mia Car';
 export const siteType = 'Person'; // schema
 export const locale = 'en_EN';
 export const lang = 'en';
 export const skipContent = 'Skip to content';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+  name: 'Mia Car', // i.e. Lene Saile - page / blog author's name. Must be set.
   avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
+  email: 'contact@miacar.net', // i.e. hola@lenesaile.com - email of the author
+  website: 'https://www.miacar.net', // i.e. https.://www.lenesaile.com - the personal site of the author
+  //fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
 };
 export const creator = {
   name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
@@ -26,11 +26,11 @@ export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light 
 export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
 export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
 export const opengraph_default_alt =
-  "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
+  "Mia Car Art"; // alt text for default meta image"
 export const blog = {
   // RSS feed
-  name: 'My Web Development Blog',
-  description: 'Tell the word what you are writing about in your blog. It will show up on feed readers.',
+  name: 'Mia Car Art',
+  description: 'Artworks by Mia Car.',
   // feed links are looped over in the head. You may add more to the array.
   feedLinks: [
     {
@@ -87,7 +87,7 @@ export const greenweb = {
       domain: domain
     }
   ],
-  services: [{domain: 'netlify.com', serviceType: 'cdn'}]
+  services: [{domain: 'cloudflare.com', serviceType: 'cdn'}]
 };
 export const tests = {
   pa11y: {
@@ -96,9 +96,9 @@ export const tests = {
     globalIgnore: []
   }
 };
-export const viewRepo = {
+/*export const viewRepo = {
   // this is for the view/edit on github link. The value in the package.json will be pulled in.
   allow: true,
   infoText: 'View this page on GitHub'
-};
-export const easteregg = true;
+};*/
+export const easteregg = false;
